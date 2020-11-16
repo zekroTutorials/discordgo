59e0ebe869ff2c66a5cd7927d35846dea6272af3
zekroTJA <riho@live.de>
2020-11-17 00:23:18 +0100

    add sources

---
+++ cmd/bot/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/zekroTutorials/discordgo/internal/config"
+)
+
+func main() {
+	const fileName = "./config/config.json"
+
+	cfg, err := config.ParseConfigFromJSONFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	s, err := discordgo.New("Bot " + cfg.Token)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = s.Open(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit...")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	s.Close()
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/zekroTutorials/discordgo
+
+go 1.15
+
+require (
+	github.com/bwmarrin/discordgo v0.22.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20201116153603-4be66e5b6582 // indirect
+	golang.org/x/sys v0.0.0-20201116194326-cc9327a14d48 // indirect
+)
